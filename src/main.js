fbd1d0bf702512818997b3a6f475c099deb16cf6
cordenbrock <cordenbrock@gmail.com>
2020-11-20 13:10:50 -0800

    Add error handling for rejected API requests and Promise execution, display errors in console and UI

--- src/main.js
+++ src/main.js
@@ -29,11 +29,18 @@ $(document).ready(function() {
 
     Exchange.getConversionRates(baseCurrencyInput)
       .then(function(response) {
+        if (response instanceof Error) {
+          throw Error(`ExchangeRate API error -- ${response.message}`);
+        }
         let conversionRates, targetCurrency, targetAmount;
         conversionRates = response.conversion_rates;
         targetCurrency = conversionRates[`${targetCurrencyInput}`];
         targetAmount = parseFloat(baseAmountInput * targetCurrency).toFixed(2);
         $("#targetAmount").val(targetAmount);
+      })
+      .catch(function(error) {
+        console.log(error.message);
+        $('#error').html(`<small style="color:red; text-align:center;">Sincerest apologies, dear currency conversion seeker, but it appears the interweb overlords are denying the entered currency conversion!<br>Please contact our chief tech consultant below who you'll notice also moonlights as our chief financial consultant.</small>`);
       });
   });
 });
