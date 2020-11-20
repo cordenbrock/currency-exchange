cc565160100bb91bab81586d7f3ed5a12646ace4
cordenbrock <cordenbrock@gmail.com>
2020-11-20 10:20:05 -0800

    Add setup event listeners function that will add form clearing functionality

--- src/main.js
+++ src/main.js
@@ -4,11 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from '../src/services/exchange.service.js';
 
-
-
+function setUpEventListeners() {
+  $("#baseCurrency").on("focus", function() {
+    $("#targetAmount").val('');
+  });
+  $("#baseAmount").on("focus", function() {
+    $("#targetAmount").val('');
+  });
+  $("#targetCurrency").on("focus", function() {
+    $("#targetAmount").val('');
+  });
+}
 
 $(document).ready(function() {
-
+  setUpEventListeners();
 
   $("#form").on("submit", function(e) {
     e.preventDefault();
