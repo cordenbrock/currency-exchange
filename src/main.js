de13ee28bbb1ac4034a9d8eeaa94c35751406bc5
cordenbrock <cordenbrock@gmail.com>
2020-11-23 09:07:15 -0800

    Refactor clearFields and sessionStorage functionality

--- src/main.js
+++ src/main.js
@@ -5,27 +5,20 @@ import './css/styles.css';
 import Exchange from '../src/services/exchange.service.js';
 
 
-function clearTargetAmountField() {
-  $("#baseCurrency").on("focus", function() {
-    $("#targetAmount").val('');
-  });
-  $("#baseAmount").on("focus", function() {
-    $("#targetAmount").val('');
-  });
-  $("#targetCurrency").on("focus", function() {
-    $("#targetAmount").val('');
-  });
-}
+
 function displayError() {
-  $('#error').html(`<small style="color:red; text-align:center;">Sincerest apologies, dear currency conversion seeker, but it appears the interweb overlords are denying the entered currency conversion!<br>Please contact our chief tech consultant below who you'll notice also moonlights as our chief financial consultant.</small>`);
+  $('#error').html(`<small style="color:red">Sincerest apologies, dear currency conversion seeker, but it appears the interweb overlords are denying the entered currency you requested to convert!<br>Please select another currency or contact our chief tech consultant below who you'll notice also moonlights as our chief financial consultant.</small>`);
 }
 
-function clearError() {
-  $("#error").empty();
+function clearFields() {
+  $("#baseCurrency, #baseAmount, #targetCurrency").on("focus", function() {
+    $("#targetAmount").val('');
+    $("#error").empty();
+  });
 }
 
 function setUpEventListeners() {
-  clearTargetAmountField();
+  clearFields();
 }
 
 $(document).ready(function() {
@@ -33,48 +26,38 @@ $(document).ready(function() {
 
   $("#form").on("submit", function(e) {
     e.preventDefault();
-    clearError();
 
-    let baseCurrencyInput, baseAmountInput, targetCurrencyInput;
+    let baseCurrencyInput, baseAmountInput, targetCurrencyInput, storageCheck;
     baseCurrencyInput = $("#baseCurrency").val();
     baseAmountInput = $("#baseAmount").val();
     targetCurrencyInput = $("#targetCurrency").val();
+    storageCheck = Object.keys(sessionStorage).includes(`rates_${baseCurrencyInput}`);
     
-    let storage = sessionStorage;
-    
-    function checkStorage() {
-      for (let key in storage) {
-        if (`rates_${baseCurrencyInput}` === key) {
-          return true;
-        }
-      }
-    }
 
-    if (checkStorage()) {
-      console.log(true);
-      let conversionRates, targetCurrency, targetAmount;
-      conversionRates = JSON.parse(storage.getItem([`rates_${baseCurrencyInput}`]));
-      targetCurrency = conversionRates[`${targetCurrencyInput}`];
+    if (storageCheck) {
+      console.log("sessionStorage is being used");
+      let storedConversionRates, targetCurrency, targetAmount;
+      storedConversionRates = JSON.parse(sessionStorage.getItem([`rates_${baseCurrencyInput}`]));
+      targetCurrency = storedConversionRates[`${targetCurrencyInput}`];
       targetAmount = parseFloat(baseAmountInput * targetCurrency).toFixed(2);
       $("#targetAmount").val(targetAmount);
     } else {
+      console.log("API service call is being used");
       Exchange.getConversionRates(baseCurrencyInput)
         .then(function(response) {
           if (response instanceof Error) {
             throw Error(`ExchangeRate API error -- ${response.message}`);
           }
-          let conversionRates, storedConversionRates;
-          conversionRates = response.conversion_rates; 
-          sessionStorage.setItem(`rates_${baseCurrencyInput}`, JSON.stringify(conversionRates));
-          storedConversionRates = JSON.parse(sessionStorage.getItem(`rates_${baseCurrencyInput}`));
-          let targetCurrency, targetAmount;
-          targetCurrency = storedConversionRates[`${targetCurrencyInput}`];
+          let conversionRates, targetCurrency, targetAmount;
+          conversionRates = response.conversion_rates;
+          targetCurrency = conversionRates[`${targetCurrencyInput}`];
           targetAmount = parseFloat(baseAmountInput * targetCurrency).toFixed(2);
           $("#targetAmount").val(targetAmount);
+          sessionStorage.setItem(`rates_${baseCurrencyInput}`, JSON.stringify(conversionRates));
         })
         .catch(function(error) {
           displayError();
-          console.log(error.message); // "console.log" usage only here for assignment to display error type
+          console.log(error);
         });
     }
   });
