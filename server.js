d7dd70ab126c5d1c29ac3c90a4aa601c54251a6e
cordenbrock <cordenbrock@gmail.com>
2020-12-02 19:13:30 -0800

    initial commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const distPath = path.join(__dirname, 'dist');
 app.use(express.static(distPath));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.join(distPath, 'index.html'));
 });
 
 app.listen(port, () => {
