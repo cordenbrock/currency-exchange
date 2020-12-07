3ff21365b1a6465c4d7f382ceaf81a24bcdeb8de
cordenbrock <cordenbrock@gmail.com>
2020-12-06 16:07:14 -0800

    Refactor, add error message, change var declarations

--- src/main.js
+++ src/main.js
@@ -4,10 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from '../src/services/exchange.service.js';
 
+function useStorageData(baseCurrencyInput, targetCurrencyInput, baseAmountInput) {
+  const storedConversionRates = JSON.parse(sessionStorage.getItem([`rates_${baseCurrencyInput}`]));
+  const targetCurrency = storedConversionRates[`${targetCurrencyInput}`];
+  const targetAmount = parseFloat(baseAmountInput * targetCurrency).toFixed(2);
+  $("#targetAmount").val(targetAmount);
+}
 
+function displayResult(response,baseCurrencyInput, targetCurrencyInput, baseAmountInput) {
+  const conversionRates = response.conversion_rates;
+  const targetCurrency = conversionRates[`${targetCurrencyInput}`];
+  const targetAmount = parseFloat(baseAmountInput * targetCurrency).toFixed(2);
+  $("#targetAmount").val(targetAmount);
+  sessionStorage.setItem(`rates_${baseCurrencyInput}`, JSON.stringify(conversionRates));
+}
 
-function displayError() {
-  $('#error').html(`<small style="color:red">Sincerest apologies, dear currency conversion seeker, but it appears the interweb overlords are denying the entered currency you requested to convert!<br>Please select another currency or contact our chief tech consultant below who you'll notice also moonlights as our chief financial consultant.</small>`);
+function displayError(error) {
+  $('#error').html(`<p>${error}</p><small style="color:red">Sincerest apologies, dear currency conversion seeker, but it appears the interweb overlords are denying the entered currency you requested to convert!<br>Please select another currency or contact our chief tech consultant below who you'll notice also moonlights as our chief financial consultant.</small>`);
 }
 
 function clearFields() {
@@ -27,37 +40,24 @@ $(document).ready(function() {
   $("#form").on("submit", function(e) {
     e.preventDefault();
 
-    let baseCurrencyInput, baseAmountInput, targetCurrencyInput, storageCheck;
-    baseCurrencyInput = $("#baseCurrency").val();
-    baseAmountInput = $("#baseAmount").val();
-    targetCurrencyInput = $("#targetCurrency").val();
-    storageCheck = Object.keys(sessionStorage).includes(`rates_${baseCurrencyInput}`);
+    const baseCurrencyInput = $("#baseCurrency").val();
+    const baseAmountInput = $("#baseAmount").val();
+    const targetCurrencyInput = $("#targetCurrency").val();
+    const storageCheck = Object.keys(sessionStorage).includes(`rates_${baseCurrencyInput}`);
     
 
     if (storageCheck) {
-      console.log("sessionStorage is being used");
-      let storedConversionRates, targetCurrency, targetAmount;
-      storedConversionRates = JSON.parse(sessionStorage.getItem([`rates_${baseCurrencyInput}`]));
-      targetCurrency = storedConversionRates[`${targetCurrencyInput}`];
-      targetAmount = parseFloat(baseAmountInput * targetCurrency).toFixed(2);
-      $("#targetAmount").val(targetAmount);
+      useStorageData(baseCurrencyInput,targetCurrencyInput,baseAmountInput);
     } else {
-      console.log("API service call is being used");
       Exchange.getConversionRates(baseCurrencyInput)
         .then(function(response) {
           if (response instanceof Error) {
             throw Error(`ExchangeRate API error -- ${response.message}`);
           }
-          let conversionRates, targetCurrency, targetAmount;
-          conversionRates = response.conversion_rates;
-          targetCurrency = conversionRates[`${targetCurrencyInput}`];
-          targetAmount = parseFloat(baseAmountInput * targetCurrency).toFixed(2);
-          $("#targetAmount").val(targetAmount);
-          sessionStorage.setItem(`rates_${baseCurrencyInput}`, JSON.stringify(conversionRates));
+          displayResult(response,baseCurrencyInput,targetCurrencyInput,baseAmountInput);
         })
         .catch(function(error) {
-          displayError();
-          console.log(error);
+          displayError(error);
         });
     }
   });
